d6df60831c703735a52d9612bf187b7ba4170258
Manuel Alejandro <mplaza005@gmail.com>
2024-04-05 16:45:59 -0500

    createListar

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -1,18 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
-   public function hola($num1,$num2){
+       
+   public function index(){
+
+      $cursos = Curso::orderBy('id', 'desc')->get();
+       return view('curso.listar', compact('cursos'));
+
+  } 
 
 
+    public function create(){
 
-    return $num1+$num2;
-
-   }
-
-
+        return view('curso.create');
+    
+     }
+  
+     public function store(Request $request){
+  
+        $curso= new Curso();
+        $curso->name=$request->name;
+        $curso->descripcion=$request->descripcion;
+        //ADJUNTAR EL PDF
+        $file=$request->file("urlPdf");
+        $nombreArchivo = "pdf_".time().".".$file->guessExtension();
+        $request->file('urlPdf')->storeAs('public/imagenes', $nombreArchivo );
+        $curso->urlPdf = $nombreArchivo;
+        $curso->save();
+        return redirect()->route('curso.index');
+  
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/curso/create',[CursoController::class,'create']);
+Route::post('/curso/store',[CursoController::class,'store'])->name('curso.store');
+Route::get('cursos/listar',[CursoController::class,'index'])->name('curso.index');
 
-Route::get('/curso/{dato1}/{dato2}',[CursoController::class,'hola'])->name('curso.show');
-
+// Route::get('/frmprimo',[CursoController::class,'formularioPrimo']);
+// Route::post('/calcularprimo',[CursoController::class,'calcularPrimo'])->name('primo.calcular');
+// Route::get('/frmproducto',[ProductController::class,'formularioProducto']);
+// Route::post('/calcularprimo',[ProductController::class,'productoStore'])->name('product.store');
+//Route::get('/curso/{dato1}/{dato2}',[CursoController::class,'hola'])->name('curso.show');
 //Route::get('curso/{curso}',[CursoController::class,'show'])->name('curso.show');
