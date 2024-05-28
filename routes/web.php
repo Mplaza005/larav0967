f299440fbd9858684ef44d46a57c804c2aafea42
Manuel Alejandro <mplaza005@gmail.com>
2024-05-28 17:23:47 -0500

    relacion mmformulario

--- routes/web.php
+++ routes/web.php
@@ -23,19 +23,20 @@ use Illuminate\Support\Facades\Route;
 //Route::get('curso/{curso}',[CursoController::class,'show'])->name('curso.show');
 
 
-Route::get('cursos/listar',[CursoController::class,'index'])->name('curso.index');
-Route::get('/curso/create',[CursoController::class,'create']);
-Route::post('/curso/store',[CursoController::class,'store'])->name('curso.store');
-Route::get('curso/{curso}',[CursoController::class,'show'])->name('curso.show');
-Route::put('curso/{curso}',[CursoController::class,'update'])->name('curso.update');
-Route::delete('curso/{curso}',[CursoController::class,'destroy'])->name('curso.destroy');
-Route::get('curso/{curso}/editar',[CursoController::class,'edit'])->name('curso.edit');
+// Route::get('cursos/listar',[CursoController::class,'index'])->name('curso.index');
+// Route::get('/curso/create',[CursoController::class,'create']);
+// Route::post('/curso/store',[CursoController::class,'store'])->name('curso.store');
+// Route::get('curso/{curso}',[CursoController::class,'show'])->name('curso.show');
+// Route::put('curso/{curso}',[CursoController::class,'update'])->name('curso.update');
+// Route::delete('curso/{curso}',[CursoController::class,'destroy'])->name('curso.destroy');
+// Route::get('curso/{curso}/editar',[CursoController::class,'edit'])->name('curso.edit');
 
 
 
 ////////////////////         PROFILE          ///////////////////////
 
-Route::get('profile/create',[ProfileController::class,'create']);
-Route::post('profile/store', [ProfileController::class,'store'])->name('profile.store');
+// Route::get('profile/create',[ProfileController::class,'create']);
+// Route::post('profile/store', [ProfileController::class,'store'])->name('profile.store');
 
-
+Route::get('usuario/asociar_rol',[RoleUserController::class,'asociar']);
+Route::post('usuario/asociar_rol/store',[RoleUserController::class,'store'])->name('role_user.store');
